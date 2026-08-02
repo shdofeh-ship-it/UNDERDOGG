99422bbe5f57d77c7eca66bf9dd1e3d3f6571821
shdofeh-ship-it <shdofeh@gmail.com>
2026-08-02 17:49:43 +0300

    Add loading progress functionality
    Implement loading progress indicator in app.js.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log('UNDERDOGG v2 started');
+const fill = document.querySelector(".loading-fill");
+const percent = document.getElementById("loadingPercent");
+
+let progress = 0;
+
+const loading = setInterval(() => {
+
+    progress++;
+
+    fill.style.width = progress + "%";
+    percent.textContent = progress;
+
+    if(progress >= 100){
+        clearInterval(loading);
+    }
+
+},25);
